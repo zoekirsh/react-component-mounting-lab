5ab5b8431d49fec8a2c4b6d752c82b3296446e2a
Zoe Kirsh <zoekirsh@gmail.com>
2021-02-04 11:08:42 -0800

    all passing

--- src/App.js
+++ src/App.js
@@ -11,14 +11,11 @@ class App extends Component {
 
 
   //Your code here:
+  componentDidMount() {
+    this.handleAddTimer()
+  }
 
-
-
-
-
-
-
-
+  
 
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
--- src/Timer.js
+++ src/Timer.js
@@ -7,6 +7,13 @@ class Timer extends Component {
   };
 
   // add your code here
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
   render() {
     const { time, color } = this.state;
